204fb8684f628aa710859f78d8445564257c7e36
Jayaram318 <jayaramireddy318@gmail.com>
2023-09-12 10:07:54 +0530

    change

--- dynamic_varibles/network.tf
+++ dynamic_varibles/network.tf
@@ -1,30 +1,29 @@
 resource "aws_vpc" "my_vpc" {
-  cidr_block = "192.168.0.0/16"
+  cidr_block       = "192.168.0.0/16"
 
-  tags = {
-    Name="from terraform"
+  tags             = {
+    Name           ="from terraform"
   }
 }
 resource "aws_subnet" "subnet1" {
-    vpc_id = aws_vpc.my_vpc.id
-    cidr_block = "192.168.0.0/24"
-    tags = {
-      Name="sub_1"
+    vpc_id         = aws_vpc.my_vpc.id
+    cidr_block     = "192.168.0.0/24" #implicit dependency
+    tags           = {
+      Name         ="sub_1"
     }
-  
-}
+  }
 resource "aws_subnet" "subnet2" {
   
-  vpc_id = aws_vpc.my_vpc.id
-  cidr_block = "192.168.1.0/24"
-  tags = {
-    Name="sub_2_from terraform"
+  vpc_id          = aws_vpc.my_vpc.id
+  cidr_block      = "192.168.1.0/24" #implicit dependency
+  tags            = {
+    Name          ="sub_2_from terraform"
   }
 }
 resource "aws_subnet" "subnet3" {
-  vpc_id = aws_vpc.my_vpc.id
-  cidr_block = "192.168.2.0/24"
-  tags = {
-    Name="subnet3_tf"
+  vpc_id          = aws_vpc.my_vpc.id
+  cidr_block      = "192.168.2.0/24" #implicit dependency
+        tags      = {
+    Name          ="subnet3_tf"
   }
 }
